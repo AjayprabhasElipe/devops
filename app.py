cbb3d0ef4943afd39355bd97136384f363792a2d
AjayprabhasElipe <ajayprabhaselipe@gmail.com>
2025-03-04 11:47:10 +0530

    Initial commit

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def register():
-    return render_template('register.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
